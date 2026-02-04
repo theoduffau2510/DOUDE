2958a7c07124f247f2c9c481e00b3f99833751e6
Théo DUFFAU <theoduffau2510@gmail.com>
2026-02-04 16:44:53 +0100

    fail

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001'
+const API_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3001'
 
 export default function Contact() {
   const [formSubmitted, setFormSubmitted] = useState(false)
